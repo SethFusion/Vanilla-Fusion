9934c8d2778afa44f143c43f5e042f686b3c2256
SethFusion <sethfusionofficial@gmail.com>
2025-05-20 15:21:43 -0500

    added credits page

--- VanillaFusion.lua
+++ VanillaFusion.lua
@@ -53,3 +53,80 @@ for _, filename in pairs(NFS.getDirectoryItems(SMODS.current_mod.path .. consuma
   end
   file()
 end
+
+SMODS.current_mod.extra_tabs = function()
+  local c_vanilla = HEX('ECE8DD')
+  return {
+    label = 'Credits',
+    tab_definition_function = function()
+    return {
+      n = G.UIT.ROOT,
+      config = {
+        r = 0.1, 
+        minw = 8, 
+        minh = 6, 
+        align = 'tl', 
+        padding = 0.2,
+        emboss = 0.05,
+        colour = G.C.BLACK
+      },
+      nodes = {{
+        n = G.UIT.R,
+        config = {
+          padding = 0.15,
+          align = 'cl'
+        },
+        nodes = {{
+          n = G.UIT.T,
+          config = {
+            text = 'Programming:',
+            scale = 0.65,
+            colour = G.C.BLUE
+          }
+        }}
+      },{
+        n = G.UIT.R,
+        config = {
+          align = 'cl'
+        },
+        nodes = {{
+          n = G.UIT.T,
+          config = {
+            text = '- Seth Fusion',
+            scale = 0.5,
+            colour = c_vanilla
+          }
+        }}
+      },{
+        n = G.UIT.R,
+          config = {
+          padding = 0.15,
+          align = 'cl'
+        },
+        nodes = {{
+          n = G.UIT.T,
+          config = {
+            text = 'Art:',
+            scale = 0.65,
+            colour = G.C.RED
+          }
+        }}
+      },{
+        n = G.UIT.R,
+          config = {
+          align = 'cl'
+        },
+        nodes = {{
+          n = G.UIT.T,
+          config = {
+            text = '- Seth Fusion: Minefield\n' .. 
+              '- gsfg: Multiverse, Kepler, Mischief',
+            scale = 0.5,
+            colour = c_vanilla
+          }
+        }}
+      }}
+    }
+    end
+  }
+end
